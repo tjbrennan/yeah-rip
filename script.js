b55e75c71c2d1b3df817650b9d3f7b2960524d8b
Taylor Brennan <taylor.j.brennan@gmail.com>
2018-03-05 23:02:45 -0800

    clean up

--- script.js
+++ script.js
@@ -1,80 +1,102 @@
-var canvas;
 var intervalTime = 10;
 var message = 'Yeah, RIP.';
-var clicks = 0;
+var logging = false;
 
 function start() {
-    var clip1 = document.getElementById('clip1');
-    var clip2 = document.getElementById('clip2');
-
-    clip2.addEventListener('playing', function() { clip1.pause(); });
-    clip2.addEventListener('ended', function() { clip1.play() });
-    clip1.setAttribute('loop', true);
+    var canvas;
+    var text;
+    var clips = new Clips();
 
     canvas = document.getElementById('canvas');
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     canvas.style.display = 'block';
 
-    var text = new BounceText();
+    text = new BounceText(canvas.getContext('2d'));
 
     canvas.addEventListener('click', function() {
-        clicks++;
-        clip2.play();
+        clips.toggle();
         text.stop();
-        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-        text = new BounceText();
+        text.start();
     });
+
+    text.start();
 }
 
-function BounceText () {
-    var ctx = canvas.getContext('2d');
+function BounceText(ctx) {
+    var speed = 1;
+    var intervalId;
 
-    ctx.font = (canvas.width / 10) + 'px Courier';
+    ctx.font = (ctx.canvas.width / 10) + 'px Courier';
     ctx.fillStyle = 'Goldenrod';
+    ctx.shadowBlur = 2;
+    
     var textSize = ctx.measureText(message);
     var textWidth = textSize.width;
-    var textHeight = textSize.width / 10; // this could be better
-
-    var speed = 1 + clicks;
-    var x = random((textWidth / 10), canvas.width - (textWidth + (textWidth / 10)));
-    var y = random(textHeight + (textHeight / 10), canvas.height - (textHeight / 10));
-    var xv = random(1, speed) * (random(0, 1) ? 1 : -1);
-    var yv = random(1, speed) * (random(0, 1) ? 1 : -1);
-
-    ctx.shadowOffsetX = -0.2 * xv;
-    ctx.shadowOffsetY = -0.2 * yv;
-    ctx.shadowBlur = 2;
-    ctx.shadowColor = random(0, 1) ? 'White' : 'Black';
-
-    ctx.fillText(message, x, y);
-
-    var interval = setInterval(function() {
-        if (x + textWidth >= canvas.width || x <= 0) {
-            xv = -xv;
-            ctx.shadowOffsetX = -0.2 * xv;
-        }
-        if (y >= canvas.height || y - textHeight <= 0) {
-            yv = -yv;
-            ctx.shadowOffsetY = -0.2 * yv;
-        }
-
-        x += xv;
-        y += yv;
-
-        ctx.fillText(message, x, y);
-        // console.log(x, y, xv, yv);
-        
-    }, intervalTime);
-
+    var textHeight = textSize.width / 10;
+    var x = random((textWidth / 10), ctx.canvas.width - (textWidth + (textWidth / 10)));
+    var y = random(textHeight + (textHeight / 10), ctx.canvas.height - (textHeight / 10));
+    
     return {
-        stop: function () {
-            clearInterval(interval);
+        start: function() {
+            var vx = random(1, speed) * (random(0, 1) ? 1 : -1);
+            var vy = random(1, speed) * (random(0, 1) ? 1 : -1);
+            var shadowMode = random(0, 10);
+
+            ctx.shadowOffsetX = -0.2 * vx;
+            ctx.shadowOffsetY = -0.2 * vy;
+            ctx.shadowColor = random(0, 1) ? 'White' : 'Black'
+
+            intervalId = setInterval(function() {
+                if (x + textWidth >= ctx.canvas.width || x <= 0) {
+                    vx = -vx;
+                    ctx.shadowOffsetX = -0.2 * vx;
+                    if (shadowMode % 2) ctx.shadowColor = random(0, 1) ? 'White' : 'Black';
+                }
+                if (y >= ctx.canvas.height || y - textHeight <= 0) {
+                    vy = -vy;
+                    ctx.shadowOffsetY = -0.2 * vy;
+                    if (shadowMode % 2) ctx.shadowColor = random(0, 1) ? 'White' : 'Black';
+                }
+        
+                x += vx;
+                y += vy;
+                if (shadowMode === 0) ctx.shadowColor = random(0, 1) ? 'White' : 'Black';
+
+                ctx.fillText(message, x, y);
+                if (logging) console.log(x, y, vx, vy);
+            }, intervalTime);
+        },
+        stop: function() {
+            clearInterval(intervalId);
+            ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+            speed++;
         }
     }
 }
 
-function random (min, max) {
+function Clips() {
+    var clip1 = document.getElementById('clip1');
+    var clip2 = document.getElementById('clip2');
+    var playing = false;
+
+    clip1.addEventListener('playing', function() { playing = true; });
+    clip1.addEventListener('pause', function() { playing = false });
+    clip1.setAttribute('loop', true);
+
+    return {
+        toggle: function() {
+            if (playing) {
+                clip1.pause();
+                clip2.play();
+            } else {
+                clip1.play();
+            }
+        }
+    }
+}
+
+function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
